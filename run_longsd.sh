d5f1cb472cd5a6c6061967db5a0581e39caf80dc
Sharon Zhou <sharon.waxseal@gmail.com>
2022-09-04 21:41:37 -0700

    add param for bash script; update todos

--- run_longsd.sh
+++ run_longsd.sh
@@ -1,2 +1,2 @@
-CUDA_VISIBLE_DEVICES=1 python longsd.py -f the_goat -n 2 &
-CUDA_VISIBLE_DEVICES=0 python longsd.py -f the_goat -n 2 && fg
+CUDA_VISIBLE_DEVICES=1 python longsd.py -f "$1" -n 2 &
+CUDA_VISIBLE_DEVICES=0 python longsd.py -f "$1" -n 2 && fg
